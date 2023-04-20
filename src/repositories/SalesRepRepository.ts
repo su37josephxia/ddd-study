0a66613702e084aa7139240a2a7a176a5bbfb3dc
josephxia <15906475@qq.com>
2023-04-20 14:50:00 +0800

    feat: add original

---
+++ src/repositories/SalesRepRepository.ts
@@ -0,0 +1,8 @@
+import SalesRep from "../entities/SalesRep";
+
+export default class SalesRepRepository {
+
+    findRepo(areaCode, operatorCode) {
+        return new SalesRep()
+    }
+}
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,5 @@
+export class UserRepository {
+    save(user) {
+        return user
+    }
+}
